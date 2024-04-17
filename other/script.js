8b1db3315a921ca000bc32a3eaed672bd1202b7f
Digz1611 <diego.langeveldt@gmail.com>
2024-04-17 12:03:19 +0200

    done, just needa add comments and format

--- other/script.js
+++ other/script.js
@@ -1,35 +1,6 @@
-// const quoteText = document.getElementById('quoteText');
-// const quoteAuthor = document.getElementById('quoteAuthor');
-// const newQuoteBtn = document.getElementById('newQuoteBtn');
-
-// function getQuote() {
-//     fetch('https://api.quotable.io/random')
-//         .then(response => response.json())
-//         .then(data => {
-//             quoteText.textContent = data.content;
-//             quoteAuthor.textContent = `- ${data.author}`;
-//         })
-//         .catch(error => {
-//             console.error('Error fetching data:', error);
-//             quoteText.textContent = 'An error occurred while fetching the quote.';
-//             quoteAuthor.textContent = '';
-//         });
-// }
-
-// newQuoteBtn.addEventListener('click', getQuote);
-
-// // Initial quote fetch
-// getQuote();
-
-
-
 const quoteText = document.getElementById('quoteText');
 const quoteAuthor = document.getElementById('quoteAuthor');
 const newQuoteBtn = document.getElementById('newQuoteBtn');
-const stickyButton = document.getElementById('stickyButton');
-const container = document.querySelector('.container');
-
-let isImageBackground = false;
 
 function getQuote() {
     fetch('https://api.quotable.io/random')
@@ -47,17 +18,15 @@ function getQuote() {
 
 newQuoteBtn.addEventListener('click', getQuote);
 
-stickyButton.addEventListener('click', function() {
-    if (isImageBackground) {
-        container.style.backgroundImage = 'none';
-        container.style.backgroundColor = 'linear-gradient(to bottom right, rgb(107, 32, 255), rgb(32, 255, 117))';
-    } else {
-        container.style.backgroundImage = 'url(misty-forest.jpg)';
-
-        container.style.backgroundColor = 'transparent';
-    }
-    isImageBackground = !isImageBackground;
-});
-
 // Initial quote fetch
 getQuote();
+
+function changeB() {
+    var element = document.getElementById("bd");
+    if (element.style.backgroundImage.includes("misty_forest.jpg")) {
+        element.style.backgroundImage = "linear-gradient(to bottom right, rgb(107, 32, 255), rgb(32, 255, 117))";
+    } else {
+        element.style.backgroundImage = "url(other/misty_forest.jpg)";
+    }
+}
+
